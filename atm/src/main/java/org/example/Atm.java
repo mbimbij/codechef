7c38d27ad1149615a9772638bed40aed24874c67
mbimbij <joseph.mbimbi@gmail.com>
2021-08-07 09:48:03 +0200

    Throw an `IllegalArgumentException` when trying to withdraw an amount greater than some upper limit, here 2000

--- atm/src/main/java/org/example/Atm.java
+++ atm/src/main/java/org/example/Atm.java
@@ -1,9 +1,16 @@
 package org.example;
 
 public class Atm {
+
+    private int withdrawUpperLimit = 2000;
+    private int withdrawLowerLimit = 0;
+
     public double withdraw(int amountToWithdraw) {
-        if (amountToWithdraw <= 0) {
-            throw new IllegalArgumentException("Amount to withdraw must be strictly positive, but got: " + amountToWithdraw);
+        if (amountToWithdraw <= withdrawLowerLimit) {
+            throw new IllegalArgumentException("Amount to withdraw must be greater than "+withdrawLowerLimit+", but got: " + amountToWithdraw);
+        }
+        if (amountToWithdraw > withdrawUpperLimit) {
+            throw new IllegalArgumentException("Amount to withdraw must be less than "+withdrawUpperLimit+", but got: " + amountToWithdraw);
         }
         return 0;
     }
--- atm/src/test/java/org/example/AtmTest.java
+++ atm/src/test/java/org/example/AtmTest.java
@@ -13,6 +13,17 @@ public class AtmTest {
         // WHEN
         Assertions.assertThatThrownBy(() -> atm.withdraw(-10))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageStartingWith("Amount to withdraw must be strictly positive");
+                .hasMessageStartingWith("Amount to withdraw must be greater than");
+    }
+
+    @Test
+    public void whenWithdrawAmountGreaterThan2000_thenException() {
+        // GIVEN
+        Atm atm = new Atm();
+
+        // WHEN
+        Assertions.assertThatThrownBy(() -> atm.withdraw(2001))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("Amount to withdraw must be less than");
     }
 }
