9a5b445bf86d639858a601e29367cc67fb86d34d
mbimbij <joseph.mbimbi@gmail.com>
2021-08-07 09:27:41 +0200

    Initial Commit

---
+++ atm/src/test/java/org/example/AppTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertThat( true ).isTrue();
+    }
+}
