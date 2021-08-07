d8a46b736995b637ad59dacad1050cf4a3b6ccb6
mbimbij <joseph.mbimbi@gmail.com>
2021-08-07 09:30:21 +0200

    Rename `App` to `Atm`

--- atm/src/test/java/org/example/AtmTest.java
+++ atm/src/test/java/org/example/AtmTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class AppTest
+public class AtmTest
 {
     @Test
     public void shouldAnswerWithTrue()
